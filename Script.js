963839afa83cdbfac1da66e0888725790081588e
StephRmz <steph_rm29@hotmail.es>
2022-08-27 12:45:22 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,19 @@
+var firstName = 'Steph';
+console.log(firstName);
+
+var lastName = 'Rmz';
+var age = 31;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+
+var _3years = 3;
+var johnMark = 'John and Mark';
+var if = 23;
